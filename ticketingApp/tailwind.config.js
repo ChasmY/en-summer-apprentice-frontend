5af28eb690aca0ba74327a13fe94b175cfe48325
ChasmY <claudiusm124@gmail.com>
2023-08-21 21:12:27 +0300

    obligatorii done

---
+++ ticketingApp/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['/ticketingApp/index.html',
+            '/ticketingApp/main.js',
+            '/ticketingApp/order.html'
+          ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
